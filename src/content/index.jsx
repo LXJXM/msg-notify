f9197f9f6cf81febe6eb692336ca583d20d1ec77
lijiaming <jiaminglics@163.com>
2023-11-24 19:20:28 +0800

    feat: 代码暂存

---
+++ src/content/index.jsx
@@ -0,0 +1,30 @@
+import ReactDOM from "react-dom/client"
+import { useState } from "react"
+import "./style.scss"
+import MainModal from "./components/main-modal"
+
+function Content() {
+	const [mainModalVisiable, setMainModalVisiable] = useState()
+	return (
+		<div className="CRX-content">
+			<div
+				className="content-entry"
+				onClick={() => setMainModalVisiable(true)}
+			></div>
+			{mainModalVisiable ? (
+				<MainModal onClose={() => setMainModalVisiable(false)} />
+			) : null}
+		</div>
+	)
+}
+
+// 创建id为CRX-container的div
+const app = document.createElement("div")
+app.id = "CRX-container"
+// 将刚创建的div插入body最后
+document.body.appendChild(app)
+// 将ReactDOM插入刚创建的div
+const crxContainer = ReactDOM.createRoot(
+	document.getElementById("CRX-container")
+)
+crxContainer.render(<Content />)
--- vite.popup.config.js
+++ vite.popup.config.js
@@ -13,7 +13,18 @@ export default defineConfig({
 		// 指定dev sever的端口号
 		port: 3000,
 		// 自动打开浏览器运行以下页面
-		open: "/"
+		open: "/",
+		// 设置反向代理
+		proxy: {
+			// 以下示例表示：请求URL中含有"/api"，则反向代理到http://localhost
+			// 例如: http://localhost:3000/api/login -> http://localhost/api/login
+			// 如果反向代理到localhost报错Error: connect ECONNREFUSED ::1:80，
+			// 则将localhost改127.0.0.1
+			"/api": {
+				target: "http://localhost/",
+				changeOrigin: true
+			}
+		}
 	},
 	resolve: {
 		alias: {
